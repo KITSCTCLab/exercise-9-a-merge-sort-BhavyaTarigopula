438fb6a5e655557f9e3c84b23e654c6a05d5a554
BhavyaTarigopula <110015414+BhavyaTarigopula@users.noreply.github.com>
2022-10-11 11:44:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,21 @@
 from typing import List
 
 def merge_sort(data) -> None:
-    if len(data) >1:
+  # Write code here
+  if len(data) >1:
     mid = (len(data))//2
     data_left = data[:mid]
     data_right = data[mid:]
     merge_sort(data_left)
     merge_sort(data_right)
  
-    a = 0 # for left_data
+    a = 0 # for left_ata
     b = 0 # for right_data
     c = 0 # for main_data
 
     while a<len(data_left) and b<len(data_right):
       if data_left[a] <= data_right[b]:
-        data[c] = data_left[a] 
+        data[c] = data_left[a]
         a+=1
       else:
         data[c] = data_right[b]
@@ -30,6 +31,7 @@ def merge_sort(data) -> None:
       b+=1
       c+=1
     return data
+
 
 # Do not change the following code
 input_data = input()
